6c4d62da2783134d1f825dd1b5fec0803ffc2931
Jaxson Deppen <jaxsondeppen@mail.weber.edu>
2022-11-03 12:21:56 -0600

    Save

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,2 +1,15 @@
 //TODO - Your ES6 JavaScript code (if any) goes here
 import "bootstrap"
+import { movies } from './movies'
+
+for(let m of movies){
+    let m_thumb = document.getElementById('m' + m.id)
+    m_thumb.innerHTML = ` 
+    <img src="${m.poster}" alt="${m.title}" class="img-thumbnail"/> 
+    
+    `
+    m_thumb.onclick = function(){
+        displayMovie(m)
+    }
+    
+}
